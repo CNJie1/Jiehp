4b625c5ea65c26dd7e01d266e1cfc64c2dd0979c
print <110807969+CNJie1@users.noreply.github.com>
2024-01-23 23:38:49 +0800

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,188 @@
+//炽羽金樽
+function Jiex1() {
+    var checkbox = document.getElementById("Jiex1");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('403251','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('403888','I32');
+  h5gg.clearResults()
+    } else {
+  h5gg.searchNumber ('403888','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('403251','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+  //至尊金龙
+  function Jiex2() {
+    var checkbox = document.getElementById("Jiex2");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('403215','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('403188','I32');
+  h5gg.clearResults()
+    } else {
+  h5gg.searchNumber ('403188','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('403251','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+  //木乃伊
+  function Jiex3() {
+    var checkbox = document.getElementById("Jiex3");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('403215','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('403598','I32');
+  h5gg.clearResults()
+    } else {
+  h5gg.searchNumber ('403598','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('403251','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+  //火箭少女
+  function Jiex4() {
+    var checkbox = document.getElementById("Jiex4");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('403215','I32','0x100000000','0x200000000')
+  h5gg.searchNumber ('405011','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('403672','I32');
+  h5gg.editAll('402188','I32');
+  h5gg.clearResults()
+
+    } else {
+  h5gg.searchNumber ('403672','I32','0x100000000','0x200000000')
+  h5gg.searchNumber ('402188','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('403215','I32');
+  h5gg.editAll('405011','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+  //特斯拉(白特)
+  function Jiex5() {
+    var checkbox = document.getElementById("Jiex5");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('330600100','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('330600800','I32');
+  h5gg.clearResults()
+
+    } else {
+  h5gg.searchNumber ('330600800','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('330600100','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+   //特斯拉(黑特)
+   function Jiex6() {
+    var checkbox = document.getElementById("Jiex6");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('330600100','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('330600900','I32');
+  h5gg.clearResults()
+
+    } else {
+  h5gg.searchNumber ('330600900','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('330600100','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+  //特斯拉(紫特)
+  function Jiex7() {
+    var checkbox = document.getElementById("Jiex7");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('330600100','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('330601000','I32');
+  h5gg.clearResults()
+
+    } else {
+  h5gg.searchNumber ('330601000','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('330600100','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+  //M4 快乐主宰
+  function Jiex8() {
+    var checkbox = document.getElementById("Jiex8");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('10100400','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('101004016','I32');
+  h5gg.clearResults()
+
+    } else {
+  h5gg.searchNumber ('101004016','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('10100400','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+  //AK甜心宝贝
+  function Jiex9() {
+    var checkbox = document.getElementById("Jiex9");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('10100100','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('101001018','I32');
+  h5gg.clearResults()
+
+    } else {
+  h5gg.searchNumber ('101004016','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('10100400','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+  //Scar 甜蜜誓约
+  function Jiex10() {
+    var checkbox = document.getElementById("Jiex10");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('10100300','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('101003024','I32');
+  h5gg.clearResults()
+
+    } else {
+  h5gg.searchNumber ('101003024','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('10100300','I32');
+  h5gg.clearResults()
+  
+    }
+  }
+  //M762 甜蜜誓约
+  function Jiex11() {
+    var checkbox = document.getElementById("Jiex11");
+    if (checkbox.checked) {
+  h5gg.searchNumber ('10100800','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('101008016','I32');
+  h5gg.clearResults()
+
+    } else {
+  h5gg.searchNumber ('101008016','I32','0x100000000','0x200000000')
+  h5gg.getResults(100)
+  h5gg.editAll('10100800','I32');
+  h5gg.clearResults()
+  
+    }
+  }
